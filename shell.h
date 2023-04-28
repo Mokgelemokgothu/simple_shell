47fa868fbc1a20180354d999f6c552e89724e872
keamogetswe3 <keamogetswegp@gmail.com>
2023-04-27 20:09:50 -0700

    HEADER FILE

--- shell.h
+++ shell.h
@@ -15,19 +15,19 @@ extern char **environ;
 
 int _strncmp(char *str1, char *str2);
 size_t _strcmp(char *str1, char *str2, size_t num);
-int _strnlen(char *ptr);
-char *_strncpy(char *dest, char *src);
-char *_strncat(char *dest, char *ch);
+int _strnlen(char *str);
+char *_strncpy(char *dest_str, char *source_str);
+char *_strncat(char *dest_str, char *chr);
 int _putchar(char c);
 int main(int count, char **arg, char **env);
 char *_get_path(char **env);
-int _values_path(char **com, char **env);
+int _values_path(char **user_coms, char **env);
 char *_getline_command(void);
 void _getenv(char **env);
-char **_get_token(char *lineptr);
-void _exit_command(char **ptr, char *lineptr, int _exit);
+char **_get_token(char *user_input);
+void _exit_command(char **user_coms, char *user_input, int _exit);
 int _fork_fun(char **path, char **prog, char **env,
-		char *lineptr, int q, int c);
+		char *user_input, int idp, int ch_tst);
 char *_strntok(char *strn, const char *delim);
 
 #endif /* SHELL_H */
